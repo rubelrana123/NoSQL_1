dd28533672d9e4cd68b1ed07755d061f5ab6e642
rubelrana123 <rubelranatpi707458@gmail.com>
2023-06-02 02:03:07 +0600

    [Done] setup express , cors, exprss, parser

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+ 
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+ 
+ 
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`🛢 Database connection successful`);
+
+    app.listen(config.port, () => {
+      console.log(`Server is  listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(`Failed to connect database`, err);
+  }
+}
+
+bootstrap();
+
+ 
