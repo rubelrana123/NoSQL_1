625cde5e907d034078c3eb0c1b16317d25ad8d29
rubelrana123 <rubelranatpi707458@gmail.com>
2023-06-11 23:43:12 +0600

    start new day

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // const express = require('express')
-import express, { Application, NextFunction, Request, Response } from "express"
-import cors from "cors";
-const app : Application = express()
+import express, { Application, NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({extended : true}))
+app.use(express.           json())
+app.use(express.urlencoded({    extended: true }))
 
-app.get('/', (req : Request, res : Response, next : NextFunction) => {
+app.get('/', (req: Request, res: any, next: NextFunction) => {
   res.send('Hello World!')
 })
 
-export default app;
+export default app
