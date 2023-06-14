04476cf65dc9cac9baf6eb9eb381bb81782e27ea
rubelrana123 <rubelranatpi707458@gmail.com>
2023-06-14 22:50:05 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 // const express = require('express')
-import express, { Application, NextFunction, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 app.use(cors())
 
-app.use(express.           json())
-app.use(express.urlencoded({    extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any, next: NextFunction) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Hello World!')
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,18 @@
- 
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
- 
- 
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`🛢 Database connection successful`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`🛢 Database connection successful`)
 
     app.listen(config.port, () => {
-      console.log(`Server is  listening on port ${config.port}`);
-    });
+      console.log(`Server is  listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(`Failed to connect database`, err);
+    console.log(`Failed to connect database`, err)
   }
 }
 
-bootstrap();
-
- 
+bootstrap()
