49135d4833e572a364a495d61a0bb11f34790d47
rubelrana123 <rubelranatpi707458@gmail.com>
2023-06-19 09:03:37 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ const app: Application = express()
 app.use(cors())
 
 app.use(express.json())
-       app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
