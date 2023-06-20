8f71fb45dea65f34cda56bfb809ba24c54ae371a
rubelrana123 <rubelranatpi707458@gmail.com>
2023-06-20 23:08:38 +0600

    [done ]create user interface modules

---
+++ src/app/modulers/users/users.interface.ts
@@ -0,0 +1,11 @@
+// import { Schema, model, connect } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+export type IUser = {
+  id: string
+  role: string
+  password: string
+  //    "createDate" : string,
+  //    "updatedDate" : string,
+  //    "studentId || adminId || facultyId" : "",
+}
