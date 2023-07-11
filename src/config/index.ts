d0a29352f194e23be4df6d6b1fe5a23bc7fe6c3e
rubelrana123 <rubelranatpi707458@gmail.com>
2023-07-11 23:49:06 +0600

    added linting setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,11 +2,7 @@ import dotenv from 'dotenv'
 import path from 'path'
 
 dotenv.config({ path: path.join(process.cwd(), '.env') })
-console.log(
-  process.env.DEFAULT_USER_PASS,
-  'process.env.DEFAULT_USER_PASS index5'
-)
-console.log(process.env.DATABASE_URL, 'process.env.DEFAULT_USER_PASS index5')
+ 
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
